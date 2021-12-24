13317c335bf938790da3bab71cd12658deb1d7cb
dianlujitao <dianlujitao@lineageos.org>
2021-12-25 01:43:08 +0800

    X2: Remove useless servicetracker HAL
    Change-Id: I13cad89a12800bbfd5b63dfa99375b896cf07796

--- device.mk
+++ device.mk
@@ -428,10 +428,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-# Service Tracker
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.servicetracker@1.2.vendor
-
 # Telephony
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
