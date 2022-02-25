66d7c66d05805d6e5f5f481f8de0b5af1eec0985
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2022-02-26 01:48:38 +0300

    X2: Disable ART debug and optimize dexpreopt  * minimize java debug info  * use everything filter for dexpreopt (AOSPA/android_vendor_pa@ab5061c)  * disable debug configs in art  * use non-debug dex2oat
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -148,6 +148,12 @@ PRODUCT_PACKAGES += \
     com.android.media.swcodec \
     libsfplugin_ccodec
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
+
 # Display/Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-impl \
